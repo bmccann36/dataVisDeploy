5901fdb8e31880c3d30ae81ca9e83905dd43de82
Brian McCann <Brian@Users-MBP.home>
2017-11-11 22:55:38 -0500

    testing with arbitrary change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">basic brian</h1>
         </header>
         <p className="App-intro">
-          content goes here
+          updated content
         </p>
       </div>
     );
